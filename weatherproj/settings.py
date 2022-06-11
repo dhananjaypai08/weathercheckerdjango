a89f4bf4ed0e199cabebfa16d693b03055db0454
Dhananjay <dhananjay2002pai@gmail.com>
2022-06-11 17:31:32 +0530

    docker setup

--- weatherproj/settings.py
+++ weatherproj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weatherproj'
 ]
 
 MIDDLEWARE = [
